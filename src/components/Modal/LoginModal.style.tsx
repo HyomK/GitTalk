82a97a77be7986eafe3b3370f536823177b35c10
Hyomk <qet557@gmail.com>
2022-09-15 18:46:49 +0900

    feat : style 파일 분리 #7

--- src/components/Modal/LoginModal.style.tsx
+++ src/components/Modal/LoginModal.style.tsx
@@ -1,7 +1,9 @@
+/** @jsxImportSource @emotion/react */
 import colors from "@/src/pages/styles/themes/colors";
 import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
-const StyledModal = styled.div<{ open: boolean }>`
+export const StyledModal = styled.div<{ open: boolean }>`
     position: absolute;
     top: 50%;
     left: 50%;
@@ -10,10 +12,70 @@ const StyledModal = styled.div<{ open: boolean }>`
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
-    width: calc(100% - 76px);
-    padding: 16px 16px 20px 16px;
+    width: 40vw;
+    padding: 2rem 2rem 2.2rem 2rem;
     border-radius: 12px;
     background-color: ${(p) => colors.white};
 
     ${(p) => !p.open && `display:none;`}
+`;
+
+export const LoginModalStyle = css`
+    .login-container {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 3rem;
+    }
+    .login-wrapper {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        padding: 0;
+        gap: 1.2rem;
+        align-items: center;
+    }
+    .login-wrapper:hover {
+        filter: brightness(0.6);
+    }
+
+    .back-btn {
+        float: left;
+        width: 20px;
+    }
+    .back-btn:hover {
+        opacity: 0.5;
+    }
+    .kakao-btn {
+        width: 50%;
+        align-self: center;
+    }
+    .github-btn {
+        background-color: black;
+        width: 50%;
+        align-self: center;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        border-radius: 3px;
+        padding: 7px;
+        font-size: 1.3rem;
+    }
+    .github-icon {
+        width: 20px;
+        height: 100%;
+        float: left;
+        margin-right: 20px;
+        align-self: center;
+        padding: 0;
+    }
+    h1 {
+        align-self: center;
+        font-size: 2rem;
+        font-weight: 500;
+        margin: 1rem;
+    }
 `;
--- src/components/Modal/LoginModal.tsx
+++ src/components/Modal/LoginModal.tsx
@@ -1,6 +1,8 @@
+/** @jsxImportSource @emotion/react */
 import React, { ReactNode } from "react";
 import styled from "@emotion/styled";
-import css from "styled-jsx/css";
+import { css } from "@emotion/react";
+import { LoginModalStyle, StyledModal } from "./LoginModal.style";
 import colors from "@/src/pages/styles/themes/colors";
 import ModalLayout from "../layouts/ModalLayout";
 
@@ -22,7 +24,7 @@ const Modal = (props: ModalProps) => {
     const { openModal, closeModal, isOpen } = props;
     return (
         <ModalLayout open={isOpen}>
-            <StyledModal open={isOpen}>
+            <StyledModal open={isOpen} css={LoginModalStyle}>
                 <div className="login-container">
                     <div className="back-btn" onClick={closeModal}>
                         <Image src={BackIcon} height={20} width={20} />
@@ -52,65 +54,6 @@ const Modal = (props: ModalProps) => {
                         </div>
                     </div>
                     <h1>Get Start!</h1>
-                    <style jsx>{`
-                        .login-container {
-                            width: 100%;
-                            display: flex;
-                            flex-direction: column;
-                            justify-content: center;
-                            gap: 3rem;
-                        }
-                        .login-wrapper {
-                            display: flex;
-                            flex-direction: row;
-                            justify-content: center;
-                            padding: 0;
-                            gap: 1.2rem;
-                            align-items: center;
-                        }
-                        .login-wrapper:hover {
-                            filter: brightness(0.6);
-                        }
-
-                        .back-btn {
-                            float: left;
-                            width: 20px;
-                        }
-                        .back-btn:hover {
-                            opacity: 0.5;
-                        }
-                        .kakao-btn {
-                            width: 50%;
-                            align-self: center;
-                        }
-                        .github-btn {
-                            background-color: black;
-                            width: 50%;
-                            align-self: center;
-                            display: flex;
-                            flex-direction: row;
-                            justify-content: center;
-                            align-items: center;
-                            color: white;
-                            border-radius: 3px;
-                            padding: 7px;
-                            font-size: 1.3rem;
-                        }
-                        .github-icon {
-                            width: 20px;
-                            height: 100%;
-                            float: left;
-                            margin-right: 20px;
-                            align-self: center;
-                            padding: 0;
-                        }
-                        h1 {
-                            align-self: center;
-                            font-size: 2rem;
-                            font-weight: 500;
-                            margin: 1rem;
-                        }
-                    `}</style>
                 </div>
             </StyledModal>
         </ModalLayout>
@@ -118,20 +61,3 @@ const Modal = (props: ModalProps) => {
 };
 
 export default Modal;
-
-const StyledModal = styled.div<{ open: boolean }>`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    width: 40vw;
-    padding: 2rem 2rem 2.2rem 2rem;
-    border-radius: 12px;
-    background-color: ${(p) => colors.white};
-
-    ${(p) => !p.open && `display:none;`}
-`;
