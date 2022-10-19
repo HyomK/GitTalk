ee11cbcef696cd979beb801d8f02d9c6d2f1ba83
Hyomk <qet557@gmail.com>
2022-10-19 21:53:55 +0900

    deploy : alias 오류 해결

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
     reactStrictMode: false,
     swcMinify: true,
     env: {
         BASE_URL: process.env.BASE_URL,
+    },
+    resolve: {
+        alias: {
+            "@/": path.resolve(__dirname, "./"),
+        },
+        extensions: [".js", ".jsx", ".css"],
     },
 };
 
